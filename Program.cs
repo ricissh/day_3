8490e1545cf3ae17787923a55ecc90f93a993b82
ricissh <rihards.stegmanis@icloud.com>
2018-05-08 19:37:09 +0300

    day3_1

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace day_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mainigais = Funkcija2();
+            Console.WriteLine(mainigais);
+            Funkcija();
+            Console.ReadLine();
+
+        }
+        static void Funkcija()
+        {
+            int a, b, c;
+            a = 7;
+            b = 9;
+
+            c = a + b;
+            Console.ReadLine();
+            Console.WriteLine(c);
+
+        }
+        static int Funkcija2()
+        {
+            int a, b, c;
+            a = 7;
+            b = 9;
+            c = a + b;
+
+            return c;
+        }
+
+    }
+
+}
